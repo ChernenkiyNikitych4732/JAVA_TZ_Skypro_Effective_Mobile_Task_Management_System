eb58057d2f4f9306d4108b86d619a22a3d1bc9ed
chern <chernenkiy.nikitychsssr@gmail.com>
2025-01-20 15:13:14 +0100

    JAVA ТЗ Skypro - EffectiveMobile

--- src/main/java/ru/chernenkiy/java_tz_skypro_effective_mobile_task_management_system/JavaTzSkyproEffectiveMobileTaskManagementSystemApplication.java
+++ src/main/java/ru/chernenkiy/java_tz_skypro_effective_mobile_task_management_system/JavaTzSkyproEffectiveMobileTaskManagementSystemApplication.java
@@ -2,7 +2,6 @@ package ru.chernenkiy.java_tz_skypro_effective_mobile_task_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class JavaTzSkyproEffectiveMobileTaskManagementSystemApplication {
